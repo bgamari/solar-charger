54d1b442a8675c949e8b281847c2cc40057387ac
Ben Gamari <bgamari.foss@gmail.com>
2013-05-31 09:57:55 -0400

    Add button initialization

--- firmware/solar-charger.c
+++ firmware/solar-charger.c
@@ -8,11 +8,23 @@ void handle_line_recv(const char* line, unsigned int length)
   usart_write(line, length);
 }
 
+int init_buttons()
+{
+  exti_select_source(EXTI8, GPIOA); // Button 3
+  exti_select_source(EXTI10, GPIOA); // Button 2
+  exti_select_source(EXTI11, GPIOA); // Button 1
+
+  exti_enable_request(EXTI8 | EXTI10 | EXTI11);
+  exti_set_trigger(EXTI8 | EXTI10 | EXTI11, EXTI_TRIGGER_FALLING);
+}
+
 int main(void)
 {
   rcc_peripheral_enable_clock(&RCC_AHBENR, RCC_AHBENR_GPIOAEN);
   rcc_peripheral_enable_clock(&RCC_AHBENR, RCC_AHBENR_GPIOBEN);
   enable_io_expander();
+
+  init_buttons();
 
   on_line_recv = handle_line_recv;
   configure_usart();
